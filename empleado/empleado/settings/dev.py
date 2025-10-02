4fe55db505671108d18e40b8344c1e197fa79a4f
Ricardo Vargas Vargas <vargasvargas.ric@gmail.com>
2025-10-02 06:03:40 -0500

    BD CONFIGURADA

--- empleado/empleado/settings/dev.py
+++ empleado/empleado/settings/dev.py
@@ -9,8 +9,12 @@ ALLOWED_HOSTS = []
 # Database
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "dbempleado",
+        "USER": "django",
+        "PASSWORD": "django",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
